9e8ed2f301add1a477671ef0780d4460ce2df157
Sergei Mikurov <454004@mail.ru>
2024-06-07 20:44:54 +0500

    Fix obviously false assert

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,10 +1,25 @@
+import os
+from tempfile import TemporaryDirectory
+
+import requests_mock
+
 from page_loader import download
+
+REQUESTED_URL = 'https://ru.hexlet.io/courses'
+HTML_FILE_NAME = 'ru-hexlet-io-courses.html'
+PAGE_DATA = 'data'
 
 
 def test_download():
-    """Test page download.
+    """Test page download function."""
+    with requests_mock.Mocker() as mock:
+        mock.get(REQUESTED_URL, text=PAGE_DATA)
 
-    TODO: fix obviously false assert
-    """
-    file_path = download('https://ru.hexlet.io/courses', '/var/tmp')
-    assert '/var/tmp/ru-hexlet-io-courses.html' == file_path
+        with TemporaryDirectory() as tmpdir:
+            current_file_path = download(REQUESTED_URL, tmpdir)
+            expected_file_path = os.path.join(tmpdir, HTML_FILE_NAME)
+            assert expected_file_path == current_file_path
+
+            with open(current_file_path) as file:
+                current_file_data = file.read()
+                assert PAGE_DATA == current_file_data
